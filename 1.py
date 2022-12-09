abfcbe9c7d4d25ca1a9a240e97bed7363b55a25f
tramhao <haoxinkui@gmail.com>
2022-12-09 15:56:12 +0800

    environment setup

--- 1.py
+++ 1.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
-x = "Hello World"
-print(x)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.core.interactiveshell import InteractiveShell
+import openpyxl
+InteractiveShell.ast_node_interactivity = "all"
+pd.set_option('max_columns',1000)
+pd.set_option('max_irow',30)
+pd.set_option("display.float_format", lambda x: '%.5f' % x)
+# %matplotlib inline
+plt.rcParams['font.sans-serif'] = 'SimHei'
+plt.rcParams['axes.unicode_minus'] = False
+# %config InlineBackend.figure_format = 'svg'
+
