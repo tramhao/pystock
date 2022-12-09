60448364453326d2f8288444e63b15a73f4f2933
tramhao <haoxinkui@gmail.com>
2022-12-09 16:16:28 +0800

    added dataframe selection

--- 1.py
+++ 1.py
@@ -2,14 +2,25 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from IPython.core.interactiveshell import InteractiveShell
+# from IPython.core.interactiveshell import InteractiveShell
 import openpyxl
-InteractiveShell.ast_node_interactivity = "all"
-pd.set_option('max_columns',1000)
-pd.set_option('max_irow',30)
+
+# InteractiveShell.ast_node_interactivity = "all"
+# pd.set_option('max_column', 1000)
+# pd.set_option('max_row', 30)
+pd.options.display.max_rows = 30
+pd.options.display.max_columns = 1000
 pd.set_option("display.float_format", lambda x: '%.5f' % x)
 # %matplotlib inline
 plt.rcParams['font.sans-serif'] = 'SimHei'
 plt.rcParams['axes.unicode_minus'] = False
 # %config InlineBackend.figure_format = 'svg'
 
+df6 = pd.DataFrame({
+    'code': [1, 2, 2, 4, 5, 5, 6],
+    'name': ['a', 'a', 'b', 'c', 'd', 'd', 'e'],
+    'freq': [2, 3, 4, 2, 1, 4, 5],
+    'value': [12, 14, 17, 20, 25, 26, 30]
+})
+
+print(df6.iloc[:, 1:3])
