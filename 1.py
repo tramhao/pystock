4011571980248842e52a5c3ffcdbca48bbff7628
tramhao <haoxinkui@gmail.com>
2022-12-11 11:14:56 +0800

    test

--- 1.py
+++ 1.py
@@ -16,11 +16,26 @@ plt.rcParams['font.sans-serif'] = 'SimHei'
 plt.rcParams['axes.unicode_minus'] = False
 # %config InlineBackend.figure_format = 'svg'
 
-df6 = pd.DataFrame({
+df1 = pd.DataFrame({
     'code': [1, 2, 2, 4, 5, 5, 6],
     'name': ['a', 'a', 'b', 'c', 'd', 'd', 'e'],
     'freq': [2, 3, 4, 2, 1, 4, 5],
     'value': [12, 14, 17, 20, 25, 26, 30]
 })
 
-print(df6.iloc[:, 1:3])
+# print(df1.iloc[:, 1:3])
+
+# df2 = df1.set_index(['code', 'name'])
+# df3 = df1.melt(id_vars=['code', 'name'], var_name=['year'], value_name='yoy')
+print(df1)
+# print(df2.stack())
+# print(df2.reset_index())
+# print(df3)
+
+
+def plusone(x):
+    return x+1
+
+
+df2 = df1['value'].apply(plusone)
+print(df2)
