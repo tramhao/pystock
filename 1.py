739d7284375ea6e00bb8f36240bd44b7ce67a8e8
tramhao <haoxinkui@gmail.com>
2022-12-12 23:09:27 +0800

    auto format works

--- 1.py
+++ 1.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 # from IPython.core.interactiveshell import InteractiveShell
 import openpyxl
 
@@ -10,18 +11,20 @@ import openpyxl
 # pd.set_option('max_row', 30)
 pd.options.display.max_rows = 30
 pd.options.display.max_columns = 1000
-pd.set_option("display.float_format", lambda x: '%.5f' % x)
+pd.set_option("display.float_format", lambda x: "%.5f" % x)
 # %matplotlib inline
-plt.rcParams['font.sans-serif'] = 'SimHei'
-plt.rcParams['axes.unicode_minus'] = False
+plt.rcParams["font.sans-serif"] = "SimHei"
+plt.rcParams["axes.unicode_minus"] = False
 # %config InlineBackend.figure_format = 'svg'
 
-df1 = pd.DataFrame({
-    'code': [1, 2, 2, 4, 5, 5, 6],
-    'name': ['a', 'a', 'b', 'c', 'd', 'd', 'e'],
-    'freq': [2, 3, 4, 2, 1, 4, 5],
-    'value': [12, 14, 17, 20, 25, 26, 30]
-})
+df1 = pd.DataFrame(
+    {
+        "code": [1, 2, 2, 4, 5, 5, 6],
+        "name": ["a", "a", "b", "c", "d", "d", "e"],
+        "freq": [2, 3, 4, 2, 1, 4, 5],
+        "value": [12, 14, 17, 20, 25, 26, 30],
+    }
+)
 
 # print(df1.iloc[:, 1:3])
 
@@ -34,8 +37,8 @@ print(df1)
 
 
 def plusone(x):
-    return x+1
+    return x + 1
 
 
-df2 = df1['value'].apply(plusone)
+df2 = df1["value"].apply(plusone)
 print(df2)
